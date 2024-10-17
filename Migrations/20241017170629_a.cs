03126c8cc6db48217f3f9814dfbe573a53042e92
user <user@WIN-MKQ26SV0D4C>
2024-10-17 20:06:37 +0300

    s

---
+++ Migrations/20241017170629_a.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class a : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MilitaryUnits_ContactPersons_ContactPersonId",
+                table: "MilitaryUnits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MilitaryUnits_ContactPersonId",
+                table: "MilitaryUnits");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactPersons_MilitaryUnitId",
+                table: "ContactPersons",
+                column: "MilitaryUnitId",
+                unique: true,
+                filter: "[MilitaryUnitId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContactPersons_MilitaryUnits_MilitaryUnitId",
+                table: "ContactPersons",
+                column: "MilitaryUnitId",
+                principalTable: "MilitaryUnits",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContactPersons_MilitaryUnits_MilitaryUnitId",
+                table: "ContactPersons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ContactPersons_MilitaryUnitId",
+                table: "ContactPersons");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MilitaryUnits_ContactPersonId",
+                table: "MilitaryUnits",
+                column: "ContactPersonId",
+                unique: true,
+                filter: "[ContactPersonId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MilitaryUnits_ContactPersons_ContactPersonId",
+                table: "MilitaryUnits",
+                column: "ContactPersonId",
+                principalTable: "ContactPersons",
+                principalColumn: "Id");
+        }
+    }
+}
